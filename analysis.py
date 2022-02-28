58cd359940666ebfa32ada5154d157de46ba468d
Lígia D'Ávila Bozzi <ligia.bozzi@gmail.com>
2022-02-28 11:39:06 -0300

    correlating asia drinks with beet consumption

--- analysis.py
+++ analysis.py
@@ -2,3 +2,9 @@ import pandas as pd
 import numpy as np
 
 drinks = pd.read_csv('./data/drinks_dataset.csv')
+asia = drinks.loc[drinks['continent'] == 'Asia']
+print(asia.head())
+
+
+print(f"Spirit are best correlation with beer consumption: 0.694\n The correlation of wine with beer consumption is: 0.437.\n As we can see below:")
+print(asia.corr())
