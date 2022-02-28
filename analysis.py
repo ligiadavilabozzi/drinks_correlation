d47948ae87acebf8d885bd9907c51430a6f02190
Lígia D'Ávila Bozzi <ligia.bozzi@gmail.com>
2022-02-28 11:43:31 -0300

    correting the beer with wine an spirit in North America

--- analysis.py
+++ analysis.py
@@ -8,3 +8,9 @@ print(asia.head())
 
 print(f"Spirit are best correlation with beer consumption: 0.694\n The correlation of wine with beer consumption is: 0.437.\n As we can see below:")
 print(asia.corr())
+
+north_america = drinks.loc[drinks['continent'] == 'North America']
+print(north_america.head())
+
+print(north_america.corr())
+print(f'It is not the same in North America. The wine are best: 0.40\n The correltion of spirit with beer is -0.073')
